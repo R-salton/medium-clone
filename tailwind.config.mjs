e35fde5a492f24dcd743e6c3f7172a213ed5b7b5
R-salton <ringusa22@gmail.com>
2025-08-08 20:34:52 +0200

    Convex Setup

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class", // Enables class-based dark mode
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/components/toggle.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
